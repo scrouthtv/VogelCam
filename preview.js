70b567559fe6c82e1911ce75316c8241862da76b
Lenni <lennivh24@gmail.com>
2020-10-20 16:13:17 +0000

    basic preview working

--- preview.js
+++ preview.js
@@ -1,9 +1,16 @@
 const prefix = "../files/"
 
 function openPreview(filename) {
-	document.getElementById("previewPane").innerHTML = "";
+	const previewPane = document.getElementById("previewPane");
 	var iframe = document.createElement("iframe");
 	iframe.src = prefix + filename;
 	iframe.title = "Preview";
-	document.getElementById("previewPane").appendChild(iframe);
+	previewPane.insertBefore(iframe, previewPane.firstChild);
+	previewPane.style.display = "block";
+}
+
+function closePreview() {
+	const previewPane = document.getElementById("previewPane");
+	previewPane.removeChild(previewPane.firstChild);
+	previewPane.style.display = "none";
 }
